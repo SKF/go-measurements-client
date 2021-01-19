15ae9aa14cb29c44444bddec73dfc22f6a47aaba
Anton Ekberg <anton.ekberg@skf.com>
2021-01-19 14:36:27 +0100

    Fix NodeMetaData type to allow null values (#5)
    * NodeMetadata is map[string]*string not map[string]string

--- scripts/patch-x-nullable.sh
+++ scripts/patch-x-nullable.sh
@@ -3,6 +3,7 @@
 OUTPUT=$(
   cat "$1" \
   | jq '.definitions.Node["x-nullable"] = false' \
+  | jq '.definitions.NodeMetaData.additionalProperties["x-nullable"] = true' \
 )
 
 [[ $? == 0 ]] && echo "${OUTPUT}" >| $1
