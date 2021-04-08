8da332a4725cf3e14d45540163002d3bc28baf2f
Rickard Englund <rickard.englund@cygni.se>
2021-04-08 15:37:20 +0200

    download measurements swagger

--- rest/example/go.mod
+++ rest/example/go.mod
@@ -9,4 +9,8 @@ require (
 	github.com/SKF/go-rest-utility v0.2.0
 	github.com/SKF/go-utility/v2 v2.12.0
 	github.com/aws/aws-sdk-go v1.35.33
+	github.com/go-openapi/errors v0.20.0 // indirect
+	github.com/go-openapi/strfmt v0.20.1 // indirect
+	github.com/go-openapi/swag v0.19.15 // indirect
+	github.com/go-openapi/validate v0.20.2 // indirect
 )
--- rest/example/main.go
+++ rest/example/main.go
@@ -8,19 +8,26 @@ import (
 	"github.com/SKF/go-rest-utility/client"
 	"github.com/SKF/go-rest-utility/client/auth"
 	"github.com/SKF/go-utility/v2/auth/secretsmanagerauth"
+	"github.com/SKF/go-utility/v2/env"
 	"github.com/SKF/go-utility/v2/stages"
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 )
 
-const serviceName = "example-service"
+const serviceName = "user_service"
 
 func main() {
 	ctx := context.Background()
+	var (
+		awsRegion = env.GetAsString("AWS_REGION", "eu-west-1")
+		awsProfile = env.GetAsString("AWS_PROFILE", "users_playground")
+	)
 
-	sess, err := session.NewSession()
-	if err != nil {
-		panic(err)
-	}
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		Config:            aws.Config{Region: &awsRegion},
+		Profile:           awsProfile,
+		SharedConfigState: session.SharedConfigEnable,
+	}))
 
 	client := hierarchy.NewClient(
 		hierarchy.WithStage(stages.StageSandbox),
@@ -31,7 +38,7 @@ func main() {
 				ServiceName:              serviceName,
 				AWSSession:               sess,
 				AWSSecretsManagerAccount: "633888256817",
-				AWSSecretsManagerRegion:  "eu-west-1",
+				AWSSecretsManagerRegion:  awsRegion,
 				SecretKey:                "user-credentials/" + serviceName,
 				Stage:                    stages.StageSandbox,
 			},
