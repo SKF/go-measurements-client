f55ef629e13c130ea56c2602835a5bf547ed84f2
Johan Dahlberg <johan@dahlis.xyz>
2021-07-16 10:24:26 +0200

    Wrong response model in get band overall (#9)

--- rest/client.go
+++ rest/client.go
@@ -17,7 +17,7 @@ type MeasurementsClient interface {
 	PostNodeData(ctx context.Context, nodeData []models.ModelNodeDataRequest) error
 	DeleteNodeData(ctx context.Context, nodeID uuid.UUID, deleteNodeDataRequest models.ModelDeleteNodeDataRequest) error
 
-	GetBandOverall(ctx context.Context, measurementID uuid.UUID, startFrequency, stopFrequency int64) (models.ModelNodeBandOverallResponse, error)
+	GetBandOverall(ctx context.Context, measurementID uuid.UUID, startFrequency, stopFrequency int64) (models.ModelMeasurementBandOverallResponse, error)
 }
 
 type client struct {
@@ -82,16 +82,16 @@ func (c *client) DeleteNodeData(ctx context.Context, nodeID uuid.UUID, deleteNod
 	return nil
 }
 
-func (c *client) GetBandOverall(ctx context.Context, measurementID uuid.UUID, startFrequency, stopFrequency int64) (models.ModelNodeBandOverallResponse, error) {
+func (c *client) GetBandOverall(ctx context.Context, measurementID uuid.UUID, startFrequency, stopFrequency int64) (models.ModelMeasurementBandOverallResponse, error) {
 	request := rest.Get("/node-data/{measurementId}/band/overall{?startFrequency,stopFrequency*}").
 		Assign("measurementId", measurementID.String()).
 		Assign("startFrequency", startFrequency).
 		Assign("stopFrequency", stopFrequency).
 		SetHeader("Accept", "application/json")
 
-	var response models.ModelNodeBandOverallResponse
+	var response models.ModelMeasurementBandOverallResponse
 	if err := c.DoAndUnmarshal(ctx, request, &response); err != nil {
-		return models.ModelNodeBandOverallResponse{}, fmt.Errorf("failed to get overall band value for measurement: %w", err)
+		return models.ModelMeasurementBandOverallResponse{}, fmt.Errorf("failed to get overall band value for measurement: %w", err)
 	}
 
 	return response, nil
